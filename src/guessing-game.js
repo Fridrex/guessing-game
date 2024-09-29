9acc6b7b6ebfd2cfd5458acfb4d57e22c0c7d049
Peter <fridrex@mail.com>
2024-09-30 00:11:06 +0100

    feat: finish task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = null;
+        this.max = null;
+        this.lastGuess = null;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.lastGuess = Math.round((this.min + this.max) / 2);
+        return this.lastGuess;
     }
 
     lower() {
-
+        this.max = this.lastGuess;
     }
 
     greater() {
-
+        this.min = this.lastGuess;
     }
 }
 
